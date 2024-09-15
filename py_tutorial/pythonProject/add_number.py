ffea7a4656c1f9640c18441ea1d815f68773dcfb
Fabliha Zafor <fablihazafar@gmail.com>
2024-09-15 21:51:36 +0600

    Add learning python project

---
+++ py_tutorial/pythonProject/add_number.py
@@ -0,0 +1,7 @@
+p = int(input('Enter a number: '))
+r = int(input('Enter another number: '))
+
+print('Output:')
+print(p + r)
+print(p - r)
+print(p * r)
---
+++ py_tutorial/pythonProject/odd_or_even.py
@@ -0,0 +1,6 @@
+n = int(input().strip())
+
+if n % 2 == 0:
+    print('Even...')
+else:
+    print('Odd...')
